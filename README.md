29e0e508b77c564d4d9478674b3a8fb4f8d56224
PengLiu <peng.liu@volvo.com>
2020-01-15 19:42:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# V-BOOT-WEB
+
+V 前端源码
+
+
+#### Build Setup
+``` bash
+# 安装依赖
+npm install
+
+# 启动服务 localhost:8013
+npm run dev
+
+# 构建生产环境
+npm run build
+# 如果上面的命令失败可以尝试
+npm run build:prod --report
+```
---
+++ src/components/Github/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="click" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Github',
+  methods: {
+    click() {
+      window.open('https://github.com/liupeng826/seed-boot', '_blank')
+    }
+  }
+}
+</script>
